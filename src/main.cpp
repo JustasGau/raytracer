c3338567f39d13f1773c5ba642a719919f713038
Justas Gaurylius <justas.gaurylius@gmail.com>
2022-06-07 20:51:12 +0300

    add jpg textures

--- src/main.cpp
+++ src/main.cpp
@@ -125,6 +125,14 @@ hittable_list two_perlin_spheres() {
     return objects;
 }
 
+hittable_list earth() {
+    auto earth_texture = make_shared<image_texture>("./src/earthmap.jpg");
+    auto earth_surface = make_shared<lambertian>(earth_texture);
+    auto globe = make_shared<sphere>(point3(0,0,0), 2, earth_surface);
+
+    return hittable_list(globe);
+}
+
 void signal_handler(int signal_num)
 {
     std::cerr << "The interrupt signal is (" << signal_num
@@ -238,6 +246,12 @@ int main(int argc, char *argv[]) {
         break;
     case 3:
         world = two_perlin_spheres();
+        lookfrom = point3(13,2,3);
+        lookat = point3(0,0,0);
+        vfov = 20.0;
+        break;
+    case 4:
+        world = earth();
         lookfrom = point3(13,2,3);
         lookat = point3(0,0,0);
         vfov = 20.0;
